c3651ec6837c87813254ab8cbfb1cdc81eff11b6
Ren Yoshida <ren.yoshida1992@gmail.com>
2021-03-07 23:13:32 +0900

    first commit

---
+++ LearnSwiftApp/LearnSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftAppApp.swift
+//  LearnSwiftApp
+//
+//  Created by 吉田廉 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
